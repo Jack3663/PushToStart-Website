09c13e76c557115f50f63749e5288de969bdb7ea
CharlieHutchinsonTheMagicalRabbit <charliehutchinson85@googlemail.com>
2016-07-20 12:45:43 +0100

    Website As It Is 20/07/2016
    Desktop Version almost finished needing content and some small changes
    
    phone and tablet versions exist in small amounts

---
+++ wwwroot/JS/AnimatedScrolling.js
@@ -0,0 +1,23 @@
+// JavaScript source code
+$(function start() {
+    $('a[href*="#"]:not([href="#"])')
+        .click(scroll);
+});
+function scroll() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+    location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+            $('html, body')
+                .animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+            $('html, body').clearQueue();
+            $('html, body, .full-width-tinted, h1, p').click(function () {
+                $('html, body').stop();
+            })
+            return false;
+        }
+    }
+}
---
+++ wwwroot/JS/NoScrollingHorizontally.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+$(function () {
+
+    var $body = $(document);
+    $body.bind('scroll', function () {
+        // "Disable" the horizontal scroll.
+        if ($body.scrollLeft() !== 0) {
+            $body.scrollLeft(0);
+        }
+    });
+
+});
