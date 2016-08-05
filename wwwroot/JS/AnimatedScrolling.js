991131e5a0c1fe2560c51b163d65f9ba67cfa97d
CharlieHutchinsonTheMagicalRabbit <charliehutchinson85@googlemail.com>
2016-08-05 14:41:19 +0100

    Comment Update 6
    Current JS Files are all Commented

--- wwwroot/JS/AnimatedScrolling.js
+++ wwwroot/JS/AnimatedScrolling.js
@@ -1,6 +1,6 @@
 // JavaScript source code
 $(function start() {
-    $('a[href*="#"]:not([href="#"])')
+    $('a[href*="#"]:not([href="#"])') //If you click an "a" that contains a href starting with # then execute scroll
         .click(scroll);
 });
 function scroll() {
@@ -9,12 +9,12 @@ function scroll() {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
         if (target.length) {
-            $('html, body')
+            $('html, body') //If the target is in the html or body then animate to it using the offset in the css and take 1 second to finish the animation
                 .animate({
                     scrollTop: target.offset().top - 70
                 }, 1000);
-            $('html, body').clearQueue();
-            $('html, body, .full-width-tinted, h1, p').click(function () {
+            $('html, body').clearQueue(); //Clear the anmation queue
+            $('html, body, .full-width-tinted, h1, p').click(function () { //if anything in the page is clicked stop the animation
                 $('html, body').stop();
             })
             return false;
